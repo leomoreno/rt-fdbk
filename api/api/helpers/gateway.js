f1e7b49d79d30af509744ea4bef03a44a267f272
Leonardo Moreno <leonardo.moreno@zagalabs.com>
2017-11-02 16:16:31 -0500

    fix(api): Fixes reply endpoint

--- api/api/helpers/gateway.js
+++ api/api/helpers/gateway.js
@@ -51,40 +51,15 @@ exports.post = function(feedback, cb) {
 
 exports.put = function(args, cb) {
     exports.initGateWay();
-
-    console.log("Updating an item...", args.uuid);
-
-    return DYNAMO_CLIENT.getItem(TABLE_NAME)
-        .setHashKey('uuid', args.uuid)
-        .execute()
-        .then(function(itemData) {
-            console.info(`Gateway::put::putItem::Success: ${JSON.stringify(itemData, null, 2)}`);
-            DYNAMO_CLIENT.putItem(
-                    TABLE_NAME,
-                    {
-                        uuid: itemData.result.uuid,
-                        createdAt: data.result.createdAt,
-                        sender: args.sender,
-                        receiver: args.receiver,
-                        type: args.type,
-                        message: args.message,
-                        reply: args.reply,
-                        updatedAt: new Date().getTime()
-                    })
-                .execute()
-                .then(function(updateData) {
-                  console.info(`Gateway::put::putItem::Success: ${JSON.stringify(updateData, null, 2)}`);
-                  cb(false, updateData.result);
-                })
-                .fail(function(e) {
-                    console.info(`Gateway::put::putItem::Error: ${JSON.stringify(e, null, 2)}`);
-                    console.info(JSON.stringify(e, null, 2));
-                    cb(e);
-                });
-        })
-        .fail(function(e) {
-            console.info(`Gateway::put::getItem::Error: ${JSON.stringify(e, null, 2)}`);
-            cb(e);
-        });
-
+    DYNAMO_CLIENT.putItem(TABLE_NAME, args)
+      .execute()
+      .then(function(updateData) {
+        console.info(`Gateway::put::putItem::Success: ${JSON.stringify(updateData, null, 2)}`);
+        cb(false, updateData.result);
+      })
+      .fail(function(e) {
+          console.info(`Gateway::put::putItem::Error: ${JSON.stringify(e, null, 2)}`);
+          console.info(JSON.stringify(e, null, 2));
+          cb(e);
+      });
 }
